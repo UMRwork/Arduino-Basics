acbeddae1d00432fd1729149e526af3a9281a10a
UMR_Work <97790149+UMRwork@users.noreply.github.com>
2022-07-19 16:30:33 +0800

    Update L298N.ino

--- Basic_Motor_With_L298N/L298N/L298N.ino
+++ Basic_Motor_With_L298N/L298N/L298N.ino
@@ -1,26 +1,65 @@
 void setup() {
-pinMode(4, OUTPUT);
-pinMode(5, OUTPUT);
-pinMode(10, OUTPUT);
-pinMode(11, OUTPUT);
+pinMode(8, OUTPUT);   //in1
+pinMode(9, OUTPUT);   //in2
+pinMode(10, OUTPUT);   //enA
+pinMode(11, OUTPUT);   //enB
+pinMode(13, OUTPUT);   //in3
+pinMode(12, OUTPUT);   //in4
 }
 
 void loop() {
-digitalWrite(4, HIGH);
-digitalWrite(5, LOW);
+//----forward
+//motor1 clockwise/anticlockwise
+digitalWrite(8, HIGH);
+digitalWrite(9,LOW);
+analogWrite(10, 255);
+//motor2 clockwise/anticlockwise
+digitalWrite(13, HIGH);
+digitalWrite(12,LOW);
+analogWrite(11, 255);
+delay(3000);          //motor1 and motor2 moves clockwise/anticlockwise for 3s
 
-digitalWrite(10, HIGH);
-digitalWrite(11, LOW);
+//----backwards
+//motor1 anticlockwise/clockwise
+digitalWrite(8, LOW);
+digitalWrite(9,HIGH);
+analogWrite(10, 255);
+//motor2 anticlockwise/clockwise
+digitalWrite(13, LOW);
+digitalWrite(12,HIGH);
+analogWrite(11, 255);
+delay(3000);          //motor1 and motor2 moves anticlockwise/clockwise for 3s
 
-analogWrite(9, 255);
-delay(1000);
-//--------------------------------------
-digitalWrite(4, LOW);
-digitalWrite(5, HIGH);
+//----left
+//motor1 anticlockwise/clockwise
+digitalWrite(8, HIGH);
+digitalWrite(9,LOW);
+analogWrite(10, 255);
+//motor2 anticlockwise/clockwise
+digitalWrite(13, LOW);
+digitalWrite(12,HIGH);
+analogWrite(11, 255);
+delay(1000);          //motor1 and motor2 turns left for 1s
 
-digitalWrite(10, LOW);
-digitalWrite(11, HIGH);
+//----right
+//motor1 anticlockwise/clockwise
+digitalWrite(8, LOW);
+digitalWrite(9,HIGH);
+analogWrite(10, 255);
+//motor2 anticlockwise/clockwise
+digitalWrite(13, HIGH);
+digitalWrite(12,LOW);
+analogWrite(11, 255);
+delay(3000);          //motor1 and motor2 turns right for 3s
 
-analogWrite(9, 255);
-delay(1000);
+//----stop
+//motor1 stop
+digitalWrite(8, LOW);
+digitalWrite(9,HIGH);
+analogWrite(10, 0);
+//motor2 stop
+digitalWrite(13, LOW);
+digitalWrite(12,HIGH);
+analogWrite(11, 0);
+delay(1000);          //motor1 and motor2 stops for 1s
 }
